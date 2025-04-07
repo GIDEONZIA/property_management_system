0b3b3edd95eaaf4f85c2390ac544ce7c2f5c9cf0
GIDEONZIA <gideonkahindi9329@gmail.com>
2025-04-07 12:13:58 +0300

    Remove bills app from project

--- property_mgmt/settings.py
+++ property_mgmt/settings.py
@@ -50,11 +50,6 @@ INSTALLED_APPS = [
     
 ]
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
 
 # Optional: collect all static files into one folder (e.g. for deployment)
 STATICFILES_DIRS = [
@@ -144,11 +139,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
